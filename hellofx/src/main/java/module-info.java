d04582d0ecf8bb3f0e9f513e773df002a5834203
dracken24 <dracken24@gmail.com>
2025-05-10 11:22:48 -0400

    fix warning

--- hellofx/src/main/java/module-info.java
+++ hellofx/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module dracken.example01 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens dracken.example01 to javafx.fxml;
     exports dracken.example01;
